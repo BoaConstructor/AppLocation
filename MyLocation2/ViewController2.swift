36d3b3a889f001b9e225932cda3074998007aff5
BoaConstructor <mr.rusin@yandex.ru>
2023-03-16 10:26:02 +0300

    Добавил табличку (пока без кода) и ViewController третьей вкладке

---
+++ MyLocation2/ViewController2.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController2.swift
+//  MyLocation2
+//
+//  Created by Apple on 16.03.2023.
+//
+
+import Foundation
+import UIKit
+
+
+class ViewController2: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        view.backgroundColor = .black
+        
+        
+      
+            
+            
+        }
+}
+
